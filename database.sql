f16d59560250c677083c082d2a2958328716d28d
danaythaa <danaythatias@gmail.com>
2026-07-28 05:03:38 +0700

    sql-kpi-summary

---
+++ database.sql
@@ -0,0 +1,30 @@
+
+SELECT 
+  'Total Loans' as metric,
+  2453 as value,
+  '' as unit
+UNION ALL
+SELECT 
+  'Total Disbursed',
+  16615000000,
+  'IDR'
+UNION ALL
+SELECT 
+  'Total Outstanding',
+  8022223568,
+  'IDR'
+UNION ALL
+SELECT 
+  'PAR Rate',
+  57.96,
+  '%'
+UNION ALL
+SELECT 
+  'Avg Loan Size',
+  6773339,
+  'IDR'
+UNION ALL
+SELECT 
+  'Non-Performing Loans',
+  1584,
+  ''
