c9e933253a4c1cde3a50b92deb8eed41869792aa
danaythaa <danaythatias@gmail.com>
2026-07-28 05:07:20 +0700

    SQL query to analyze loan data by quality OJK

---
+++ dataset.sql
@@ -0,0 +1,10 @@
+
+SELECT 
+  `kualitas_ojk_bulan_ini`,
+  COUNT(*) as loan_count,
+  SUM(`os_total`) as outstanding_amount,
+  ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (), 2) as pct_of_loans
+FROM read_files('/Volumes/__databricks_internal_catalog_genie_files_7474652793626715/01f13e72d2d21a11adaa6f6502700987/76897870652803-01f13e72d2d21a11adaa6f6502700987/b2fc6917-a45c-47fd-a435-6286c6104aae', dateFormat => "yyyy-MM-dd", delimiter => ",", escape => "\"", format => "csv", header => "true", ignoreLeadingWhiteSpace => "true", ignoreTrailingWhiteSpace => "true", inferColumnTypes => "true", mergeSchema => "true", multiLine => "true", schemaEvolutionMode => "none", schemaHints => "`regional` string, `area_id` integer, `branch_id` integer, `area` string, `branch` string, `bp` string, `nama_bp_uk` string, `majelis` string, `borrower_id` integer, `nama_mitra` string, `loan_id` integer, `tanggal_disbursement` date, `plafond` integer, `reason_high_risk` string, `angsuran_minggu_berjalan` integer, `tenor` integer, `usia_loan` integer, `status_par` integer, `total_par` integer, `dpd` integer, `jumlah_tunggakan` integer, `jumlah_minggu_angsuran` integer, `jadwal_pelayanan` string, `os_total` integer, `os_lancar` integer, `hadir` integer, `tidak_hadir` integer, `tanggal_first_par` string, `tanggal_terakhir_par` string, `loan_cycle` integer, `status_par_last_month` string, `angsuran_per_minggu` integer, `angsuran_pokok` integer, `angsuran_profit` integer, `tanggal_terakhir_bayar` date, `last_partial_payment_date` string, `last_partial_payment_amount` integer, `jumlah_saldo_partial` integer, `sisa_frequency_bayar_lebih` integer, `saldo_bayar_lebih` integer, `no_telpon_mitra` string, `alamat_mitra` string, `kualitas_ojk_bulan_ini` string, `kualitas_ojk_bulan_lalu` string, `last_presence` string, `link_koordinat` string, `nomor_hp_afin` string, `tanggal_register_afin` date, `status_mitra` string", skipRows => "0") AS portfolio_data
+WHERE `kualitas_ojk_bulan_ini` IS NOT NULL
+GROUP BY `kualitas_ojk_bulan_ini`
+ORDER BY `kualitas_ojk_bulan_ini`
